04e6036faf7efff1e31b64d8c89e98c38b1535b2
Data2ThePPL <shearer.keri@gmail.com>
2015-12-19 20:10:53 -0500

    added columns and statuses and ids. Removed old statuses

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213192841) do
+ActiveRecord::Schema.define(version: 20151220005825) do
 
   create_table "statuses", force: :cascade do |t|
-    t.string   "name"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "statuses", ["user_id"], name: "index_statuses_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
